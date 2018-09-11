04c1cd17502c130e1749c1f951a19ea81d251f95
amadoukane96 <amadoukane96@gmail.com>
2018-09-11 10:05:01 -0400

    imports order

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-import dash
 import math
-import dash_core_components as dcc
-import dash_html_components as html
+
 import pandas as pd
 import flask
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 import dateutil.parser
 
 from sfManager import sf_Manager
